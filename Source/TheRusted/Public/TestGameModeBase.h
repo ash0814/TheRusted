814769bee8c9b2e3e28561b9e927c6584041cdb7
tlfh04 <tlfh04@gmail.com>
2024-09-12 18:07:01 +0900

    Create TestPlayer , TestGameModeBase
    Reeeeeeeeeed

---
+++ Source/TheRusted/Public/TestGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THERUSTED_API ATestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
