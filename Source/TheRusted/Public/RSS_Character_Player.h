670123c256247dd1ab4c7117b313b5020ee4efbd
SangSeonRo <williamro2.2@gmail.com>
2024-09-23 16:38:11 +0900

    ADD BP_Player_Muriel_RSS Modify [RSS_Character_Player] Delete Unused uassets

--- Source/TheRusted/Public/RSS_Character_Player.h
+++ Source/TheRusted/Public/RSS_Character_Player.h
@@ -10,106 +10,21 @@ UCLASS()
 class THERUSTED_API ARSS_Character_Player : public APlayer_Base
 {
 	GENERATED_BODY()
-
 public:
-	// Sets default values for this character's properties
 	ARSS_Character_Player();
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-//INPUT
 public:
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputMappingContext* IMC_Player;
+	void BeginPlay() override;
+	void Attack();
+	void StrongAttack();
+	void Ultimate();
 	
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Move;
+	void SpawnBullet();
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Attack")
+	TSubclassOf<class ABullet_Muriel> magazine;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Turn;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Attack")
+	TArray<TSubclassOf<class ABullet_Muriel>> magazines;
 
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_LookUp;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement )
-	bool bIsInvertLookUp = true;
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Jump;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Attack1;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Attack2;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Input")
-	class UInputAction* IA_Skill1;
-
-	void Input_Turn(const struct FInputActionValue& inputValue);
-	void Input_LookUp(const struct FInputActionValue& inputValue);
-	void Input_Move(const struct FInputActionValue& inputValue);
-	void Input_Jump(const struct FInputActionValue& inputValue);
-	void Input_Attack1(const struct FInputActionValue& inputValue);
-	void Input_Attack2(const struct FInputActionValue& inputValue);
-	void Input_Skill1(const struct FInputActionValue& inputValue);
-
-//AnimMantage
-public:
-	UAnimInstance* AnimInstance;
-	
-	UPROPERTY(EditAnywhere, Category = Animation)
-	UAnimMontage* AM_Attack1;
-	
-	UPROPERTY(EditAnywhere, Category = Animation)
-	UAnimMontage* AM_Attack2;
-
-	UPROPERTY(EditAnywhere, Category = Animation)
-	UAnimMontage* AM_Skill1;
-
-	
-	void MontagePlay(UAnimMontage* animMontage);
-
-//Move
-private:
-	UPROPERTY(VisibleAnywhere, Category = "Movement")
-	FVector MoveDirection;
-
-//CAMERA
-public:
-	UPROPERTY(VisibleAnywhere, Category = Camera)
-	class USpringArmComponent* SpringArmComponent;
-
-	UPROPERTY(VisibleAnywhere, Category = Camera)
-	class UCameraComponent* CameraComponent;
-
-//Character Status
-	const int32 MaxHealthPoint = 100;
-	const int32 MaxEnergyPoint = 90;
-	const int32 MaxShieldPoint = 3;
-	const int32 MaxUltimateSkillPoint = 1500;
-
-	int32 CurrentHealthPoint = 100 ;
-	int32 CurrentEnergyPoint = 90;
-	int32 CurrentShieldPoint = 3;
-	int32 CurrentUltimateSkillPoint = 0;
-
-//Attack
-	UPROPERTY(EditDefaultsOnly, Category = PlayerSetting)
-	TSubclassOf<class ARSS_Projectile_Muriel_Attack1> Attack1_Factory;
-
-	UPROPERTY(EditAnywhere)
-	class UParticleSystem* VFX_Spawn_Attack1;
-	
-	void Excute_Attack1();
-
-	FTransform Calc_AttackTransform();
+	UFUNCTION(BlueprintCallable)
+	void ApplyDamage(float amount);
 };
