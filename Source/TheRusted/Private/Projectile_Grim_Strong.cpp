ecff0f4824d3f9d7d786cd236f41026cfac3049a
SangSeonRo <williamro2.2@gmail.com>
2024-09-26 17:59:36 +0900

    Add Grim Projectiles

---
+++ Source/TheRusted/Private/Projectile_Grim_Strong.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile_Grim_Strong.h"
+
---
+++ Source/TheRusted/Public/Projectile_Grim_Strong.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projectile_Particle_Cascade.h"
+#include "Projectile_Grim_Strong.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THERUSTED_API AProjectile_Grim_Strong : public AProjectile_Particle_Cascade
+{
+	GENERATED_BODY()
+	
+};
