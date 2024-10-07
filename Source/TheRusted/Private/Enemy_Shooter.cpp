ac4d822db0a5f717a523690d45f42a5775d9eb9c
Zerohn <zerohn96@gmail.com>
2024-10-07 10:52:44 +0900

    Change Shooter SkeletalMesh

--- Source/TheRusted/Private/Enemy_Shooter.cpp
+++ Source/TheRusted/Private/Enemy_Shooter.cpp
@@ -8,7 +8,7 @@
 AEnemy_Shooter::AEnemy_Shooter()
 {
 	MainWeapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Main Weapon"));
-	//MainWeapon->SetRelativeLocationAndRotation(FVector(-3.0f, 3.0f, 0.0f), FRotator(-1.0f, -120.0f, 10.0f));
+	//MainWeapon->SetRelativeLocationAndRotation(FVector(-2.0f, 2.0f, -3.0f), FRotator(11.0f, -130.0f, 3.0f));
 	MainWeapon->SetupAttachment(GetMesh(), FName("Weapon"));
 }
 
@@ -25,5 +25,5 @@ void AEnemy_Shooter::Tick(float DeltaTime)
 void AEnemy_Shooter::Attack()
 {
 	Super::Attack();
-	GetWorld()->SpawnActor<AProjectile_Base>(Magazine, Calc_AttackTransform(FName("head")));
+	GetWorld()->SpawnActor<AProjectile_Base>(Magazine, 	MainWeapon->GetSocketTransform(FName("muzzle")));
 }
